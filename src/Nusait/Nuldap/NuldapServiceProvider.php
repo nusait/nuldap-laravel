1591aa05983d77a080b61bca584857332f8804d2
Steve DiDomenico <steve@northwestern.edu>
2016-06-20 13:53:37 -0500

    changing serviceprovider to match 1.0 version of Nusait/Nuldap/NuLdap

--- src/Nusait/Nuldap/NuldapServiceProvider.php
+++ src/Nusait/Nuldap/NuldapServiceProvider.php
@@ -28,7 +28,7 @@ class NuldapServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('Nusait\Nuldap\Contracts\LdapInterface', function ($app) {
+        $this->app->singleton('Nusait\Nuldap\NuLdap', function ($app) {
             $config = $app['config']->get('ldap');
             if ($config['fake']) {
                 $faker = \App::make(Faker::class);
@@ -39,8 +39,8 @@ class NuldapServiceProvider extends ServiceProvider
             return new NuLdap($config['rdn'], $config['password'], $config['host'], $config['port']);
         });
 
-        $this->app->alias('Nusait\Nuldap\Contracts\LdapInterface', 'ldap');
-        $this->app->alias('Nusait\Nuldap\Contracts\LdapInterface', 'nuldap');
+        $this->app->alias('Nusait\Nuldap\NuLdap', 'ldap');
+        $this->app->alias('Nusait\Nuldap\NuLdap', 'nuldap');
     }
 
     /**
@@ -50,6 +50,6 @@ class NuldapServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['ldap', 'nuldap', 'Nusait\Nuldap\Contracts\LdapInterface'];
+        return ['ldap', 'nuldap', 'Nusait\Nuldap\NuLdap'];
     }
 }
